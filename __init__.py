70eb2a9601de11428379d180e05a0afda942b773
zagk <junvin@gmail.com>
2026-04-19 17:15:25 +0900

    Clean up __init__.py by removing comments
    Removed unnecessary comments and cleaned up the init file.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,3 @@
-"""
-ComfyUI Multiline String Node - 패키지 초기화 파일
-이 파일을 custom_nodes/multiline_string_node/ 폴더에 넣으세요.
-"""
-
 from .multiline_string_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 WEB_DIRECTORY = "./js"
